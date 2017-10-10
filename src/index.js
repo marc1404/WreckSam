e09290684eb75c75e86c2ac0750a8b4c4a16295c
Marc Vornetran <marc.vornetran@gmail.com>
2017-10-10 20:49:13 +0200

    Add Sentry error tracking with RavenJS

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import 'babel-polyfill';
+import Raven from 'raven-js';
+import RavenVue from 'raven-js/plugins/vue';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
 import router from './router';
 import Game from './Game.vue';
 
-Vue.use(VueRouter);
+if (process.env.NODE_ENV === 'production') {
+    Raven
+        .config('https://63f778bcaa9545749f8187b7dde759f6@sentry.io/228420')
+        .addPlugin(RavenVue, Vue)
+        .install();
+}
 
-Vue.component('Game', Game);
+Raven.context(bootstrap);
 
-new Vue({
-    el: '#app',
-    components: {
-        App
-    },
-    render: h => h('app'),
-    router: router
-});
+function bootstrap() {
+    Vue.use(VueRouter);
+
+    Vue.component('Game', Game);
+
+    new Vue({
+        el: '#app',
+        components: {
+            App
+        },
+        render: h => h('app'),
+        router: router
+    });
+}

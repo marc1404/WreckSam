680bb02205679bee376099314db354d4956063a1
Marc Vornetran <marc.vornetran@gmail.com>
2017-10-10 16:03:28 +0200

    Tissue and rotating bacteria

--- src/game/TutorialState.js
+++ src/game/TutorialState.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser-ce';
+import { tissueColor, bacteriaColor } from './constants';
 
 export default class TutorialState extends Phaser.State {
 
@@ -7,11 +8,24 @@ export default class TutorialState extends Phaser.State {
     }
 
     create() {
-        console.log('TutorialState.create');
+        console.log(this);
+
+        this.stage.backgroundColor = tissueColor;
+
+        const graphics = this.game.add.graphics(0, 0);
+
+        graphics.beginFill(bacteriaColor);
+        graphics.drawEllipse(0, 0, 50, 30);
+        graphics.endFill();
+
+        this.bacteria = this.game.add.sprite(this.world.centerX, this.world.centerY, graphics.generateTexture());
+
+        graphics.destroy();
+        this.bacteria.anchor.set(0.5);
     }
 
     update() {
-
+        this.bacteria.rotation += 0.01;
     }
 
 }
---
+++ src/game/constants.js
@@ -0,0 +1,7 @@
+const tissueColor = 0xffcadc;
+const bacteriaColor = 0x7a6dd6;
+
+export {
+    tissueColor,
+    bacteriaColor
+};

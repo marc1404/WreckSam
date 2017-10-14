08d8f4a3c0c52c9985b37a66388aadba014e478a
Marc Vornetran <marc.vornetran@gmail.com>
2017-10-15 01:12:35 +0200

    Work on including all cells

--- src/game/TutorialState.js
+++ src/game/TutorialState.js
@@ -3,7 +3,11 @@ import { tissueColor, bacteriaColor, boneMarrowColor } from './constants';
 
 export default class TutorialState extends Phaser.State {
 
-    bacteria = null;
+    health = {
+        bar: null,
+        text: null,
+        percent: 100
+    };
 
     tissue = {
         bacteria: null,
@@ -12,6 +16,7 @@ export default class TutorialState extends Phaser.State {
     };
 
     boneMarrow = {
+        backdrop: null,
         macrophage: null,
         neutrophil: null
     };
@@ -26,28 +31,148 @@ export default class TutorialState extends Phaser.State {
     create() {
         console.log(this);
 
+        this.createHealth();
+        this.createTissue();
+        this.createBoneMarrow();
+    }
+
+    createHealth() {
+        this.health.bar = this.game.add.graphics(0, 0);
+        this.health.text = this.game.add.text(1, 0, '100%', { font: 'normal 12pt Arial' });
+    }
+
+    createSprite(x, y, key) {
+        const sprite = this.game.add.sprite(x, y, key);
+
+        sprite.anchor.set(0.5);
+
+        return sprite;
+    }
+
+    createBacteria(x, y) {
+        return this.createSprite(x, y, 'bacteria');
+    }
+
+    createMacrophage(x, y) {
+        return this.createSprite(x, y, 'macrophage');
+    }
+
+    createNeutrophil(x, y) {
+        return this.createSprite(x, y, 'neutrophil');
+    }
+
+    createTissue() {
         this.stage.backgroundColor = tissueColor;
 
-        this.bacteria = this.game.add.sprite(this.world.centerX, this.world.centerY, 'bacteria');
+        this.createTissueBacteria();
+        this.createTissueMacrophageAnimation();
 
-        this.bacteria.anchor.set(0.5);
-        this.bacteria.scale.setTo(0.2);
+        this.createTissueMacrophage();
+        this.createTissueNeutrophil();
+    }
+
+    createTissueBacteria() {
+        const bacteria = this.createBacteria(this.world.centerX, 100);
+        bacteria.rotation += 0.5;
+        bacteria.data.rotation = 0.01;
+
+        const scale = (this.world.width * 0.6) / bacteria.width;
+        bacteria.scale.setTo(scale);
+
+        this.tissue.bacteria = bacteria;
+    }
+
+    createTissueMacrophage() {
+        const x = this.world.centerX * 0.6;
+        const macrophage = this.createMacrophage(x, this.world.centerY);
+        macrophage.rotation -= 0.8;
+
+        const scale = (this.world.width * 0.5) / macrophage.width;
+        macrophage.scale.setTo(scale);
+
+        this.tissue.macrophage = macrophage;
+    }
+
+    createTissueMacrophageAnimation() {
+        this.time.events.loop(Phaser.Timer.QUARTER * 2, () => {
+            const { macrophage } = this.tissue;
+            const key = macrophage.key === 'macrophage' ? 'macrophage_active' : 'macrophage';
+
+            macrophage.loadTexture(key);
+        }, this);
+    }
+
+    createTissueNeutrophil() {
+        const x = this.world.centerX * 1.4;
+        const y = this.world.centerY * 1.25;
+        const neutrophil = this.createNeutrophil(x, y);
+
+        const scale = (this.world.width * 0.4) / neutrophil.width;
+        neutrophil.scale.setTo(scale);
+
+        this.tissue.neutrophil = neutrophil;
+    }
+
+    createBoneMarrow() {
+        this.createBoneMarrowBackdrop();
+        this.createBoneMarrowMacrophage();
+        this.createBoneMarrowNeutrophil();
+    }
+
+    createBoneMarrowBackdrop() {
+        const graphics = this.game.add.graphics(0, 0);
+
+        graphics.beginFill(boneMarrowColor);
+        graphics.drawRect(0, 0, this.world.width, this.world.height / 5);
+        graphics.endFill();
+
+        this.boneMarrow.backdrop = this.game.add.sprite(0, this.world.height - this.world.height / 5, graphics.generateTexture());
+
+        graphics.destroy();
+    }
+
+    createBoneMarrowMacrophage() {
+        const { backdrop } = this.boneMarrow;
+        const x = backdrop.centerX / 2;
+        const macrophage = this.createMacrophage(x, backdrop.centerY);
+
+        const scale = (backdrop.height * 0.8) / macrophage.height;
+        macrophage.scale.setTo(scale);
+
+        this.boneMarrow.macrophage = macrophage;
+    }
+
+    createBoneMarrowNeutrophil() {
+        const { backdrop, macrophage } = this.boneMarrow;
+        const x = backdrop.width - macrophage.x;
+        const neutrophil = this.createNeutrophil(x, backdrop.centerY);
+
+        const scale = (backdrop.height * 0.9) / neutrophil.height;
+        neutrophil.scale.setTo(scale);
+
+        this.boneMarrow.neutrophil = neutrophil;
     }
 
     update() {
-        this.bacteria.rotation += 0.01;
+        this.tissue.neutrophil.rotation += 0.02;
+
+        const { bacteria } = this.tissue;
+        bacteria.rotation += bacteria.data.rotation;
+
+        if (bacteria.rotation < 0.25 || bacteria.rotation > 0.75) {
+            bacteria.data.rotation *= -1;
+        }
+
+        if (this.health.percent < 0) {
+            this.health.percent = 0;
+        }
+
+        this.health.text.text = Math.floor(this.health.percent) + '%';
+
+        this.health.bar.clear();
+        this.health.bar.beginFill(0xaf111c);
+        this.health.bar.drawRect(0, 0, this.world.width * (this.health.percent / 100), 20);
+        this.health.bar.endFill();
     }
 
 }
-
-/*
-    const graphics = this.game.add.graphics(0, 0);
-
-    graphics.beginFill(bacteriaColor);
-    graphics.drawEllipse(0, 0, 50, 30);
-    graphics.endFill();
-
-    this.bacteria = this.game.add.sprite(this.world.centerX, this.world.centerY, graphics.generateTexture());
-
-    graphics.destroy();
- */

9dfb364c4fd44e9f8afaea6ee43c973ad90acc19
Marc Vornetran <marc.vornetran@gmail.com>
2017-10-14 17:46:36 +0200

    Simplify constants module

--- src/game/constants.js
+++ src/game/constants.js
@@ -1,7 +1,2 @@
-const tissueColor = 0xffcadc;
-const bacteriaColor = 0x7a6dd6;
-
-export {
-    tissueColor,
-    bacteriaColor
-};
+export const tissueColor = 0xffcadc;
+export const bacteriaColor = 0x7a6dd6;

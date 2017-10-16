7026017bf49e535bae49584be5aab8bd715a0599
Jop <jopbrandenburg@gmail.com>
2017-10-16 17:04:25 +0200

    decreased the amount of text in macrophage box

--- src/modals/MacrophageModal.vue
+++ src/modals/MacrophageModal.vue
@@ -3,19 +3,16 @@
         <div class="modal-body">
             <p class="mb-0">
                 <strong>Macrophages</strong> are your first line of defense.
-                These large cells <span class="text-success">feast on bacteria cells</span>. <br />
+                These large cells <span class="text-success">feast on bacteria cells</span>
             </p>
 
             <div class="text-center">
                 <img style="width: 200px" src="assets/sprites/macrophage.png" />
             </div>
-
             <p class="mb-0">
-            In order to attack the <span class="text-danger">bacteria</span>, the <strong>body tissue</strong>, represented by the pink area, needs to contain some <span class="text-success">Macrophages</span>.
+            Touch the <strong>Macrophage</strong> in the white area at the bottom of your screen, which is the <strong>bone marrow</strong> in order to deploy 100 Macrophages to the <strong>body tissue</strong>, where they will attack the bacteria.
             </p>
-            <p>
-            Touch the <strong>Macrophage</strong> in the white area at the bottom of your screen, which is the <strong>bone marrow</strong> in order to deploy 100 Macrophages to the <strong>body tissue.</strong>
-            </p>
+            <br />
             <p>
             Touch the <strong>Macrophage</strong> in the <strong>bone marrow</strong> multiple times to deploy 100 Macrophages each time, thus reinforcing your offence. And remember: Offence is always the best defence.
             </p>
